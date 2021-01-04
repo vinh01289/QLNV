6412fa321ca71c18e3950daba6abb7471d18a182
Tran Van Vinh <vinhtran01289@gmail.com>
2021-01-04 12:37:50 +0700

    Add project files.

---
+++ QLNVSolution/Models/Information.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLNVSolution.Models
+{
+    public class Information
+    {
+        public int Id { get; set; }
+        public string HoTen { get; set; }
+
+        public DateTime NamSinh  { get; set; }
+        public string DiaChi { get; set; }
+    }
+}
---
+++ QLNVSolution/Reponsitory/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QLNVSolution.Models;
+
+namespace QLNVSolution.Reponsitory
+{
+    public class DBContext:DbContext
+    {
+
+        public DBContext(DbContextOptions<DBContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Information> Information { get; set; }
+
+    }
+}
